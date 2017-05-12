edaba4fe34f21e39f5fba1051d218f43dc9f0449
Coldly <1191152277@qq.com>
2017-05-12 15:46:55 +0800

    热修复

--- app/src/main/java/com/hc/essay/joke/MainActivity.java
+++ app/src/main/java/com/hc/essay/joke/MainActivity.java
@@ -1,14 +1,10 @@
 package com.hc.essay.joke;
 
 
-import android.os.Environment;
-import android.widget.Toast;
-
 import com.example.framelibrary.BaseSkinActivity;
 import com.mrl.baselibrary.ExceptionCrashHandler;
 
 import java.io.File;
-import java.io.IOException;
 
 public class MainActivity extends BaseSkinActivity {
     @Override
@@ -31,21 +27,9 @@ public class MainActivity extends BaseSkinActivity {
         // 获取上次的崩溃信息
         File crashFile = ExceptionCrashHandler.getInstance().getCrashFile();
         if(crashFile.exists()){
-        // 上传到服务器，后面再说.......
+        // 上传到服务器
 
 
         }
-
-        //每次启动去后台差分包 fix.apatch 然后修复本地BUG
-        //直接获取本地内存卡里的fix.apatch   （测试）
-        File fixFile=new File(Environment.getExternalStorageDirectory(),"fix.apatch");
-        try {
-            MyApplication.mPatchManager.addPatch(fixFile.getAbsolutePath());
-            Toast.makeText(this,"修复成功",Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            Toast.makeText(this,"修复失败",Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-
     }
 }
--- app/src/main/java/com/hc/essay/joke/MyApplication.java
+++ app/src/main/java/com/hc/essay/joke/MyApplication.java
@@ -2,9 +2,14 @@ package com.hc.essay.joke;
 
 import android.app.Application;
 import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.widget.Toast;
 
 import com.alipay.euler.andfix.patch.PatchManager;
 import com.mrl.baselibrary.ExceptionCrashHandler;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Email:1191152277@qq.com
@@ -32,5 +37,15 @@ public class MyApplication extends Application{
         }
         // 加载 patch 包
         mPatchManager.loadPatch();
+        //每次启动去后台差分包 fix.apatch 然后修复本地BUG
+        //直接获取本地内存卡里的fix.apatch   （测试）
+        File fixFile=new File(Environment.getExternalStorageDirectory(),"fix.apatch");
+        try {
+            MyApplication.mPatchManager.addPatch(fixFile.getAbsolutePath());
+            Toast.makeText(this,"修复成功",Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this,"修复失败",Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
 }
